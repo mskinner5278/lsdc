44562c363a706ac2a89055098dd27bd328311d9e
Michael Skinner <m.skinner5278@gmail.com>
2023-02-28 12:14:04 -0500

    Re-enable loop center button for testing

--- lsdcGui.py
+++ lsdcGui.py
@@ -2865,7 +2865,7 @@ class ControlMain(QtWidgets.QMainWindow):
           self.centeringComboBox.setDisabled(True)
           self.beamsizeComboBox.setDisabled(True)
           annealButton.setDisabled(True)
-          centerLoopButton.setDisabled(True)
+          #centerLoopButton.setDisabled(True)
           clearGraphicsButton.setDisabled(True)
           saveCenteringButton.setDisabled(True) 
           selectAllCenteringButton.setDisabled(True) 
